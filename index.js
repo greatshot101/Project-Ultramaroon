e756c0072f68733ce6b857f46e764cbf3b010ecc
WaterWolf5918 <87424826+WaterWolf5918@users.noreply.github.com>
2023-03-02 15:48:48 -0500

    Change - changed node-static for express
    Removed node-static in favor of express due to security concerns.
    
    node-static depends on minimist that then depends on optimist that has not been updated since 2014

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
-import nodeStatic from 'node-static';
+import express from 'express'
+import path from 'path'
+import * as url from 'url';
 
-
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const bare =  createServer('/bare/');
-const serve = new nodeStatic.Server('static');
-
+const app = express().use(express.static(path.join(__dirname,'/static')))
 const server = http.createServer();
+
+app.use((err, req, res, next) => {
+	console.error(err.stack)
+	res.status(500).send('Something broke!')
+})
+
+app.get('/', (req,res) => {
+	res.sendFile('/static/index.html')
+})
 
 server.on('request', (req, res) => {
     if (bare.shouldRoute(req)) {
 		bare.routeRequest(req, res);
 	} else {
-		serve.serve(req, res);
+		try{
+			app(req,res)
+		}catch( err ){
+			console.error(err)
+			console.error("File was not found")
+		}
 	}
 });
 
